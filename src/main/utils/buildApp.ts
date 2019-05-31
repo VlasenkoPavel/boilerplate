6c78453002714290faae3dce3ee7176c5cfb79d7
VlasenkoPavel <psvlasenko@gmail.com>
2019-05-31 20:27:15 +0300

    in progress

--- src/main/utils/buildApp.ts
+++ src/main/utils/buildApp.ts
@@ -1,22 +1,30 @@
 import { configDir } from './configDir';
 import { ConfigFileChain, ConfigFactory, LogConfig, ServerConfig, PostgresConfig } from '@chaika/config';
-import { LoggerFactory, ExpressLauncher, ExpressServer, TypeOrmLogger, TypeOrmConnector } from '@chaika/app-components';
+import {
+    LoggerFactory,
+    ExpressLauncher,
+    ExpressServer,
+    TypeOrmLogger,
+    TypeOrmConnector,
+    ErrorHandlingMiddleware
+} from '@chaika/app-components';
 import { ApplicationBuilder } from '@chaika/application';
 
 export const buildApp = async () => {
     const builder = await new ApplicationBuilder(ExpressLauncher)
-            .buildComponent(new ConfigFileChain(configDir, process.env.SM_ENV as string))
-            .buildWithParams(ConfigFactory, ['configFileChain'])
-            .buildConfigs([LogConfig, ServerConfig, PostgresConfig]);
+        .buildComponent(new ConfigFileChain(configDir, process.env.SM_ENV as string))
+        .buildWithParams(ConfigFactory, ['configFileChain'])
+        .buildConfigs([LogConfig, ServerConfig, PostgresConfig]);
 
     const app = builder
-            .buildWithParams(LoggerFactory, ['logConfig'])
-            .setParameter('category', 'app')
-            .buildByFactory('loggerFactory', ['category'])
-            .buildComponent(ExpressServer)
-            .buildComponent(TypeOrmLogger)
-            .buildComponent(TypeOrmConnector)
-            .create();
+        .buildWithParams(LoggerFactory, ['logConfig'])
+        .setParameter('category', 'app')
+        .buildByFactory('loggerFactory', ['category'])
+        .buildComponent([ErrorHandlingMiddleware], 'middlewares')
+        .buildComponent(ExpressServer)
+        .buildComponent(TypeOrmLogger)
+        .buildComponent(TypeOrmConnector)
+        .create();
 
     return app;
 };
